26c01002b199b9f39db3843d606dd52d24df0d89
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-08-08 22:58:17 +0530

    modified:   src/trpc/procedures/files.procedure.ts

--- src/trpc/procedures/files.procedure.ts
+++ src/trpc/procedures/files.procedure.ts
@@ -1,12 +1,12 @@
 import { db } from "@/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import { filesTable } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
 export const filesRouter = createTRPCRouter({
     getFiles: protectedProcedure.query(async ({ctx}) => {
-    const files = await db.select().from(filesTable).where(eq(filesTable.userId, ctx.auth.user.id))
+    const files = await db.select().from(filesTable).where(eq(filesTable.userId, ctx.auth.user.id)).orderBy(desc(filesTable.createdAt))
 
-    return files  
+    return files
   })
 })
